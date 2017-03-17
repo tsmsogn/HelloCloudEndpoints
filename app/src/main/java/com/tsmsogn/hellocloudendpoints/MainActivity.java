e48bb01f0339c337ad3f438cf074d37349cc70a5
tsmsogn <tsmsogn@gmail.com>
2017-03-17 18:08:44 +0900

    Implement HelloCloudEndpoints

--- app/src/main/java/com/tsmsogn/hellocloudendpoints/MainActivity.java
+++ app/src/main/java/com/tsmsogn/hellocloudendpoints/MainActivity.java
@@ -1,13 +1,61 @@
 package com.tsmsogn.hellocloudendpoints;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.tsmsogn.myapplication.backend.myApi.MyApi;
+import com.example.tsmsogn.myapplication.backend.myApi.model.MyBean;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text1);
+
+        new SayHiAsyncTask() {
+            @Override
+            protected void onPostExecute(String s) {
+                mTextView.setText(s);
+            }
+        }.execute("Cloud Endpoints");
+    }
+
+    private class SayHiAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                MyBean response = getApiClient().sayHi(params[0]).execute();
+                return response.getData();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "";
+            }
+        }
+    }
+
+    private MyApi getApiClient() {
+        return new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                new AndroidJsonFactory(), null)
+                .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                    @Override
+                    public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                        abstractGoogleClientRequest.setDisableGZipContent(true);
+                    }
+                })
+                .build();
     }
 }
